046aa9786c951050a1a5ee612fb1ce904e934904
Cyril Waechter <cyrwae@hotmail.com>
2023-04-30 11:53:43 +0200

    update example to produce files in cache
    - produce file in cache folder
    - set a specific language/country
    - open folder after creation

--- examples/generate_ifc_project_libraries.py
+++ examples/generate_ifc_project_libraries.py
@@ -1,7 +1,26 @@
-from materialsdb import cache
+import os
+import platform
+import subprocess
+from materialsdb import cache, config
 from materialsdb.ifc import project_library
 
-cache.update_producers_data()
-for producer in cache.producers():
-    file = project_library.create_project_library_from_xml(str(producer))
-    file.write(producer.with_suffix(".ifc").name)
+
+def open_folder(path):
+    if platform.system() == "Windows":
+        os.startfile(path)
+    elif platform.system() == "Darwin":
+        subprocess.Popen(["open", path])
+    else:
+        subprocess.Popen(["xdg-open", path])
+
+def update_cached_library(lang, country):
+    config.set_lang(lang)
+    config.set_country(country)
+    cache.update_producers_data()
+    for producer in cache.producers():
+        file = project_library.create_project_library_from_xml(str(producer))
+        ifc_folder = cache.get_cache_folder() / "IFC4" / f"{lang}_{country}"
+        file.write(ifc_folder / producer.with_suffix(".ifc").name)
+    open_folder(ifc_folder)
+
+update_cached_library("fr", "CH")
